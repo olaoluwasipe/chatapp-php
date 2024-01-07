09fb1dca89373099158f2b49d36552daa3bda1ee
olaoluwasipe <123474859+olaoluwasipe@users.noreply.github.com>
2024-01-07 19:22:25 +0100

    logout code changed

--- chatapp/functions/logout.php
+++ chatapp/functions/logout.php
@@ -1,21 +1,9 @@
 <?php 
 session_start();
-if(isset($_SESSION['unique_id'])) {
-    require_once "config.php";
-    $user_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
-    if(isset($user_id)) {
-        $status = "Offline now";
-        $que = $conn->query("UPDATE users SET status = '{$status}' WHERE unique_id = '$user_id'");
-        if($que) {
-            session_unset();
-            session_destroy();
-            header("Location: ../login.php");
-        }
-    } else {
-        header("location: ../users.php");
-    }
-} else {
-    header("Location: ../login.php");
-}
+require_once "../Classes/Auth.php";
+$auth = new Auth($conn);
+$logout = $auth->login($_GET);
+
+echo $logout;
 
 ?>
